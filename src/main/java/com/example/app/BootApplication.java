e1f191d14f26f3be40e5b31db81f279bee9520f6
Danil <21nash@mail.ru>
2023-05-13 12:55:28 +0300

    Initial commit

---
+++ src/main/java/com/example/app/BootApplication.java
@@ -0,0 +1,13 @@
+package com.example.pp_3_1_2_boot_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pp_3_1_2_boot_crud/Pp312BootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pp_3_1_2_boot_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp312BootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
